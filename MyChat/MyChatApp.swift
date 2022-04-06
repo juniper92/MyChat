381498ee43eee012d60818fbacb590378965b382
juniper <heyjuniper92@gmail.com>
2022-04-06 10:15:59 +0900

    Initial Commit

---
+++ MyChat/MyChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyChatApp.swift
+//  MyChat
+//
+//  Created by Mila on 2022/04/06.
+//
+
+import SwiftUI
+
+@main
+struct MyChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
