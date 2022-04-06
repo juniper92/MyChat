0c2ea6bb2b020d5d00e41d43260fe90e519bb868
juniper <heyjuniper92@gmail.com>
2022-04-06 16:25:23 +0900

    - add customtabbar & tabbarbutton

---
+++ MyChat/Helpers/Colors.swift
@@ -0,0 +1,41 @@
+//
+//  Colors.swift
+//  MyChat
+//
+//  Created by Mila on 2022/04/06.
+//
+
+import SwiftUI
+
+extension Color {
+    
+    struct Palette {
+        static var Background: Color {
+            return Color("background")
+        }
+        
+        static var ButtonPrimary: Color {
+            return Color("button-primary")
+        }
+        
+        static var IconsPrimary: Color {
+            return Color("icons-primary")
+        }
+        
+        static var IconsSecondary: Color {
+            return Color("icons-secondary")
+        }
+        
+        static var TextButton: Color {
+            return Color("text-button")
+        }
+        
+        static var TextInput: Color {
+            return Color("text-input")
+        }
+        
+        static var TextPrimary: Color {
+            return Color("text-primary")
+        }
+    }
+}
